5f88007f1c1cff6e1eab6c70fe3f945b497b8770
Dima Denysenko <coding@dimdnk.com>
2022-04-30 19:23:10 +0300

    feat: use records for all possible DTOs

--- src/main/java/org/ujar/basics/rest/helloworld/dto/ErrorResponse.java
+++ src/main/java/org/ujar/basics/rest/helloworld/dto/ErrorResponse.java
@@ -1,11 +1,8 @@
 package org.ujar.basics.rest.helloworld.dto;
 
 import java.util.List;
-import lombok.Getter;
 
-@Getter
 public record ErrorResponse(List<Error> errors) {
-  @Getter
   public record Error(String message) {
   }
 }
--- src/main/java/org/ujar/basics/rest/helloworld/web/HelloWorldController.java
+++ src/main/java/org/ujar/basics/rest/helloworld/web/HelloWorldController.java
@@ -12,8 +12,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.ujar.basics.rest.helloworld.dto.ErrorResponse;
 import org.ujar.basics.rest.helloworld.dto.GreetingDto;
+import org.ujar.starter.rest.web.dto.ErrorResponse;
 
 @RestController
 @Tag(name = "Hello World controller", description = "API for greeting")

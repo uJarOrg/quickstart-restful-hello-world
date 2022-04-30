19f44ea59744f4d682b06373adc14ef25b458be4
Dima Denysenko <coding@dimdnk.com>
2022-04-30 17:43:37 +0300

    Update Greeting.java

--- src/main/java/org/ujar/basics/rest/helloworld/dto/Greeting.java
+++ src/main/java/org/ujar/basics/rest/helloworld/dto/Greeting.java
@@ -1,7 +1,10 @@
 package org.ujar.basics.rest.helloworld.dto;
 
 public class Greeting {
+  
+  private static final String MESSAGE = "Hello, World!";
+  
   public String getMessage() {
-    return "Hello, World!";
+    return MESSAGE;
   }
 }

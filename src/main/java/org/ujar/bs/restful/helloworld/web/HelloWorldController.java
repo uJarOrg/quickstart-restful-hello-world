2e349208ee0f64ced859cd43e452421c3bce9acc
Dima Denysenko <coding@dimdnk.com>
2023-01-20 16:08:16 +0200

    chore: use shorter package naming

--- src/main/java/org/ujar/bs/restful/helloworld/web/HelloWorldController.java
+++ src/main/java/org/ujar/bs/restful/helloworld/web/HelloWorldController.java
@@ -1,4 +1,4 @@
-package org.ujar.basics.restful.helloworld.web;
+package org.ujar.bs.rst.helloworld.web;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -12,7 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.ujar.basics.restful.helloworld.web.dto.GreetingDto;
+import org.ujar.bs.rst.helloworld.web.dto.GreetingDto;
 import org.ujar.boot.starter.restful.web.dto.ErrorResponse;
 
 @RestController

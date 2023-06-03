b63a07847bf48281bc75a99b8cd9c3af0c89beb8
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2023-06-03 18:45:46 +0300

    chore: enable reasonable defaults for observability components
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/main/java/org/ujar/restfulhelloworld/RestfulHelloWorldApplication.java
+++ src/main/java/org/ujar/restfulhelloworld/RestfulHelloWorldApplication.java
@@ -2,10 +2,13 @@ package org.ujar.restfulhelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class RestfulHelloWorldApplication {
   public static void main(String[] args) {
-    SpringApplication.run(RestfulHelloWorldApplication.class, args);
+    SpringApplication springApplication = new SpringApplication(RestfulHelloWorldApplication.class);
+    springApplication.setApplicationStartup(new BufferingApplicationStartup(2048));
+    springApplication.run(args);
   }
 }
--- src/main/java/org/ujar/restfulhelloworld/config/ApplicationConfig.java
+++ src/main/java/org/ujar/restfulhelloworld/config/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package org.ujar.restfulhelloworld.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "Hello World App", version = "23.0.0"))
 class ApplicationConfig {
 }

b1e641c4bcf6538c7af4114b79647dc0f139c31c
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2025-01-30 19:09:55 +0200

    chore: update streamline version and copyright headers

--- src/main/java/com/iqkv/incubator/quickstart/mvcresthelloworld/config/ApplicationConfig.java
+++ src/main/java/com/iqkv/incubator/quickstart/mvcresthelloworld/config/ApplicationConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 IQKV Team.
+ * Copyright 2025 IQKV Team.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,6 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Hello World App", version = "24.0.0"))
+@OpenAPIDefinition(info = @Info(title = "Hello World App", version = "25.0.0"))
 class ApplicationConfig {
 }
